d7b92417d3de83733f1271c17161b3344f29d96e
Daniela Plascencia <daniela.plascencia@canonical.com>
2024-04-12 10:08:26 +0200

    tests: pin deployed charms to a CKF 1.8 compatible version
    This commit pins all charms that get deployed from Charmhub to a compatible version to
    the CKF 1.8 release. This commit also refactors the test file so it takes all versions and other
    constants from a constants.py file
    
    Part of canonical/bundle-kubeflow#863

---
+++ tests/integration/constants.py
@@ -0,0 +1,33 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+"""Constants module including constants used in tests."""
+from pathlib import Path
+
+import yaml
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+CHARM_ROOT = "."
+ENVOY_APP_NAME = "envoy"
+
+MLMD = "mlmd"
+MLMD_CHANNEL = "1.14/stable"
+MLMD_TRUST = False
+
+ISTIO_OPERATORS_CHANNEL = "1.17/stable"
+ISTIO_PILOT = "istio-pilot"
+ISTIO_PILOT_TRUST = True
+ISTIO_PILOT_CONFIG = {"default-gateway": "kubeflow-gateway"}
+ISTIO_GATEWAY = "istio-gateway"
+ISTIO_GATEWAY_APP_NAME = "istio-ingressgateway"
+ISTIO_GATEWAY_TRUST = True
+ISTIO_GATEWAY_CONFIG = {"kind": "ingress"}
+
+PROMETHEUS_K8S = "prometheus-k8s"
+PROMETHEUS_K8S_CHANNEL = "1.0/stable"
+PROMETHEUS_K8S_TRUST = True
+GRAFANA_K8S = "grafana-k8s"
+GRAFANA_K8S_CHANNEL = "1.0/stable"
+GRAFANA_K8S_TRUST = True
+PROMETHEUS_SCRAPE_K8S = "prometheus-scrape-k8s"
+PROMETHEUS_SCRAPE_K8S_CHANNEL = "1.0/stable"
+PROMETHEUS_SCRAPE_CONFIG = {"scrape_interval": "30s"}
