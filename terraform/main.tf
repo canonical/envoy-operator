c8c0258922d1cc36937856b82c24b380c9a8ad51
mvlassis <manos.vlassis@canonical.com>
2024-09-19 16:51:05 +0300

    Add Terraform module

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+resource "juju_application" "envoy" {
+  charm {
+    name     = "envoy"
+    channel  = var.channel
+    revision = var.revision
+  }
+  config             = var.config
+  model              = var.model_name
+  name               = var.app_name
+  resources          = var.resources
+  trust              = true
+  units              = 1
+}
