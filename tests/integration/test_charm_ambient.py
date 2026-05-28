4916d9addb46cc7e05ecafbba1c2a86265e3a320
Manos Vlassis <57320708+mvlassis@users.noreply.github.com>
2026-05-28 11:20:53 +0300

    chore: Update CI to use charmcraftcache (#210)
    * chore: Update CI to use charmcraftcache
    
    * Add back on_pull_request.yaml for backport label population
    
    * ci: always collect juju status
    
    * Update Cilium config: add cni-exclusive patch and update comment URL
    
    * fix: Grant actions: read to ci-tests job in release workflow
    
    The ci.yaml reusable workflow's build and release jobs request
    'actions: read' (needed by charmcraftcache), but the caller job
    in release.yaml did not grant this permission, causing a startup
    failure. Add permissions: actions: read to the ci-tests job.
    
    * fix: Grant contents: read to ci-tests job in release workflow
    
    (cherry picked from commit c83824a6ec063c5d13102ec28e88a209bae5fc28)

--- tests/integration/test_charm_ambient.py
+++ tests/integration/test_charm_ambient.py
@@ -46,12 +46,16 @@ def lightkube_client() -> Client:
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test):
-    charm = await ops_test.build_charm(CHARM_ROOT)
+async def test_build_and_deploy(ops_test, request):
+    entity_url = (
+        await ops_test.build_charm(CHARM_ROOT)
+        if not (entity_url := request.config.getoption("--charm-path"))
+        else entity_url
+    )
     image_path = METADATA["resources"]["oci-image"]["upstream-source"]
     resources = {"oci-image": image_path}
 
-    await ops_test.model.deploy(charm, resources=resources, trust=True)
+    await ops_test.model.deploy(entity_url, resources=resources, trust=True)
     await ops_test.model.deploy(MLMD.charm, channel=MLMD.channel, trust=MLMD.trust)
     await ops_test.model.integrate(ENVOY_APP_NAME, MLMD.charm)
 
